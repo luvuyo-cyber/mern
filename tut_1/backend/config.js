0b585f04e40c6e211701b2a529c3dce721171e39
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-06-27 23:38:27 +0200

    tut1

---
+++ tut_1/backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://luvuyomazibuko5275:negroswan97@bookstore-mern.f6nalzb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN'
