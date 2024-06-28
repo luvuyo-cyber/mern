af2bf51b0d158ce44e26fce51dca9d8dda90343b
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-06-29 00:40:03 +0200

    updates

--- tut_1/frontend/src/pages/CreateBooks.jsx
+++ tut_1/frontend/src/pages/CreateBooks.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import { useSnackbar } from 'notistack';
 
 const CreateBooks = () => {
 
@@ -12,6 +13,8 @@ const CreateBooks = () => {
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
     const data = {
@@ -24,11 +27,13 @@ const CreateBooks = () => {
       .post("http://localhost:5555/books", data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Created Successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error happened. Please check console!");
+        // alert("An error happened. Please check console!");
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
--- tut_1/frontend/src/pages/EditBook.jsx
+++ tut_1/frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
 
@@ -14,6 +15,8 @@ const EditBook = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
+
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     setLoading(true);
@@ -43,11 +46,13 @@ const EditBook = () => {
       .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Edited Successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error happened. Please check console!");
+        // alert("An error happened. Please check console!");
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
